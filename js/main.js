b01dce8038b73b70c9897b9ad999d0e2f8c67f4b
Zvezegor <ferocaritas@gmail.com>
2022-08-15 16:47:16 +0300

    finished

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 $(document).ready(function () {
   $(".owl-carousel").owlCarousel({
     loop: true,
-    margin: 10,
+    margin: 12,
     nav: true,
+    navClass: ["slider__nav--prev", "slider__nav--next"],
     responsive: {
       0: {
         items: 1,
       },
-      600: {
+      700: {
         items: 3,
       },
-      1000: {
-        items: 5,
+      900: {
+        items: 4,
+      },
+      1200: {
+        items: 4,
       },
     },
   });
